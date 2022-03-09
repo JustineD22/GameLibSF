cb21498baf51d61b6f53d87326c3f36de18c54c8
Justine <98328323+JustineD22@users.noreply.github.com>
2022-03-09 12:15:16 +0100

    default controller

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,18 +5,32 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class DefaultController extends AbstractController
 {
-    #[Route('/default', name: 'default')]
+    #[Route('/index', name: 'index')]
     public function index(): Response
     {
-        $urlHome = $this->generateUrl('default');
 
         return $this->render('default/index.html.twig', [
-            'urlHome' => $urlHome,
             'controller_name' => 'DefaultController',
         ]);
     }
+    #[Route('/contact', name: 'contact')]
+    public function contact() : Response
+    {
+        return $this->render('default/contact.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+
+    #[Route('/displaygames', name: 'displaygames')]
+    public function displaygames() : Response
+    {
+        return $this->render('default/displaygames.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+
+
 }
