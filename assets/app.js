ec4034fd3a83af31ac6902d8c7c953a7fab3c1f3
Justine <98328323+JustineD22@users.noreply.github.com>
2022-03-09 12:23:34 +0100

    app js

--- assets/app.js
+++ assets/app.js
@@ -10,8 +10,6 @@ const $ = require('jquery');
 require('bootstrap');
 
 // any CSS you import will output into a single css file (app.css in this case)
-import '../vendor/twbs/bootstrap/dist/css/bootstrap.css'
 import './styles/app.css';
 
 // start the Stimulus application
-import './bootstrap';
